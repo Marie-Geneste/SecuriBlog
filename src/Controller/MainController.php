0d74593c044cafe2ab2a7bf04282722ea169263e
Marie-Geneste <marie.e.geneste@gmail.com>
2024-02-15 16:32:17 +0100

    WIP Css homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main', methods: ['GET'])]
+    public function index(ArticleRepository $ArticleRepository): Response
     {
         $pageActive = 'home';
+        $articles = $ArticleRepository->findAll();
         return $this->render('main/index.html.twig', [
             'pageActive' => $pageActive,
+            'articles' => $articles
         ]);
     }
 }
